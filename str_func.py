9e4cc3429392be73f6976fb36f8085947c264b8c
ilya <ilyamalahov038@gmail.com>
2023-12-13 21:24:09 +0300

    Add function

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def function(phrase):
     phrase = input()
     return phrase.upper()
+
+"""Функция, которая делает заглавными первые буквы каждого слова в строке, поступившей на вход функции"""
+
+def title(phrase):
+    phrase = input()
+    return phrase.title()
